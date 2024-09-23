02a80a30ad0ed82b02772f1012e3842c1f63f3c2
Leo <leokh0713@gmail.com>
2024-09-23 15:43:10 -0400

    replacing function)

--- grade_calc.py
+++ grade_calc.py
@@ -8,25 +8,20 @@ midterm_1 = input("Enter grade for Midterm 1: ")
 midterm_2 = input("Enter grade for Midterm 2: ")
 final_grade = input("Enter grade for Final Exam: ") 
 midterm_and_prep_grade = input("Enter grade for Midterms and Final Preparation: ")
-def lab_programming_problems(num_of_labs):
-	if (num_of_labs >= 6):
-		return(100*0.2)
-	else:
-		return(((num_of_labs)/5)*0.2)
-def quizzes (num_of_quizzes):
-	if (num_of_quizzes < 6):
-		return((num_of_quizzes / 6)*0.15)
-	else:
-		return(100 * 0.15)
-def assignments(assignment_1, assignment_2, assignment_3, assignment_4):
-	return(((assignment_1 + assignment_2 + assignment_3 + assignment_4)/ 4) * 0.16)
-def midterms (midterm_1, midterm_2):
-	return (((midterm_1+midterm_2)/2)*0.25)
-def final (final_grade):
-	return(final_grade*0.18)
-def midterm_and_prep (midterm_and_prep_grade):
-	return(midterm_and_prep_grade *0.06)
-def grade (lab_programming_problems, quizzes, assignments, midterms, final, midterm_and_prep):
-	return(lab_programming_problems + quizzes + assignments + midterms + final + midterm_and_prep)*100
-print("You're average is: " grade + "%")
+labs = 0
+if (num_oflabs >=6): 
+	labs = 100*0.2
+else:
+	labs = (num_of_labs/6) * 0.20	
+quizs = 0
+if (num_of_quizs >= 6):
+	quizs = 100 * 0.15
+else: 
+	quizs = (num_of_quizs/6)*0.15
+assignments = ((assignment_1 + assignment_2 + assignment_3 + assignment_4)/4)*0.16
+midterms = ((midterm_1 + midterm_2)/2) *0.25
+final = final_grade * 0.18
+midterm_prep = midterm_and_prep * 0.06
+grade = labs + quizs + assignments + midterms + final + midterm_prep
+print("Your average is: "+ grade + "%")
 
