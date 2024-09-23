d91b007b6a16a30e40684f14c880d8562f1c7382
Leo <leokh0713@gmail.com>
2024-09-23 16:11:37 -0400

    fixing bugs

--- grade_calc.py
+++ grade_calc.py
@@ -7,21 +7,21 @@ assignment_4 = input("Enter grade for Assignment 4: ")
 midterm_1 = input("Enter grade for Midterm 1: ")
 midterm_2 = input("Enter grade for Midterm 2: ")
 final_grade = input("Enter grade for Final Exam: ") 
-midterm_and_prep_grade = input("Enter grade for Midterms and Final Preparation: ")
-labs = 0
-if (num_of_labs >=6): 
+midterm_and_prep = input("Enter grade for Midterms and Final Preparation: ")
+labs = 1000000000
+if (float(num_of_labs) >=6): 
 	labs = 100*0.2
 else:
-	labs = (num_of_labs/6) * 0.20	
-quizs = 0
-if (num_of_quizs >= 6):
+	labs = (float(num_of_labs)/6) * 0.20	
+quizs = 1000000000
+if (float(num_of_quizs) >= 6):
 	quizs = 100 * 0.15
 else: 
-	quizs = (num_of_quizs/6)*0.15
-assignments = ((assignment_1 + assignment_2 + assignment_3 + assignment_4)/4)*0.16
-midterms = ((midterm_1 + midterm_2)/2) *0.25
-final = final_grade * 0.18
-midterm_prep = midterm_and_prep * 0.06
+	quizs = (float(num_of_quizs)/6)*0.15
+assignments = ((float(assignment_1) + float(assignment_2) + float(assignment_3) + float(assignment_4))/4)*0.16
+midterms = ((float(midterm_1) + float(midterm_2))/2) *0.25
+final = (float(final_grade) * 0.18)
+midterm_prep = float(midterm_and_prep) * 0.06
 grade = labs + quizs + assignments + midterms + final + midterm_prep
-print("Your average is: "+ grade + "%")
+print("Your average is: "+ str(grade) + "%")
 
